fc21b197e57ba6febe03a73a6a32467981421128
Chungzter <freezycry@protonmail.com>
2025-10-22 22:50:46 +0330

    add a basic test to test_config.py

--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -1,5 +1,4 @@
-from unittest.mock import patch
-
-import pytest
-
 from commizard import config
+
+def test_gen_request_url():
+    assert config.gen_request_url() == "http://127.0.0.1:11434/api/generate"
