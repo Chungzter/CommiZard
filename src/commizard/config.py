5756c6feee0b35ed7894ca814fd3c9129237aa3f
Chungzter <freezycry@protonmail.com>
2025-10-22 23:30:42 +0330

    add set_url() prototype
    This function will be crucial to our work once we start adding online
    support for the program: we can set the configs with correct parameters
    for the rest of the program to use them

--- src/commizard/config.py
+++ src/commizard/config.py
@@ -5,8 +5,15 @@ OLLAMA_DEFAULT_GEN_ENDPOINT: str = "api/generate"
 GEN_ENDPOINT: str = OLLAMA_DEFAULT_GEN_ENDPOINT
 # url selection to be implemented in the future, this is just a placeholder
 # right now.
-
 LLM_URL: str = OLLAMA_DEFAULT_URL
+
+
+def set_url(url: str):
+    """
+    Set the LLM's URL to use
+    """
+    global LLM_URL
+    LLM_URL = url
 
 
 def gen_request_url() -> str:
